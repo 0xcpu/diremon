a77f44308f39ef52b002d07ec474accea273c9e4
cpu <cornel.punga@gmail.com>
2025-05-03 18:52:57 +0200

    Initial Commit

---
+++ diremon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  diremon
+//
+//  Created by cpu on 03.05.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
